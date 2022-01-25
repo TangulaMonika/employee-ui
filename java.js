343266a57938aac1933eeb78f031d3ec3ec01ef1
TangulaMonika <m.tangula@manuhindia.com>
2022-01-25 12:16:22 +0530

    commit

--- java.js
+++
@@ -1,9 +0,0 @@
-function error() {
-	  let signin = document.forms["form"]["signin"].value;
-	  let password = document.forms["form"]["password"].value;
-	  if (signin == "" || password=="") {
-		 
-	    alert("signin and password must be required");
-	    return false;
-	  }
-	}
